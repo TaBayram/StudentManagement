7db143fbd05818d358f3d1bc4b4cc082236e7f39
TaBayram <TaBayram@users.noreply.github.com>
2020-12-07 20:28:44 +0300

    Basic FX

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,30 +1,48 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Controller {
 
 
+    public ListView ListView_Department;
+
+    @FXML
     public void testclicked(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
 
-        String url = "jdbc:sqlserver://localhost:1433;" + "databaseName=StudentDatabase;integratedSecurity=true";
-        //String url ="jdbc:sqlserver://GREENANGEL\\;databaseName=StudentDatabase;integratedSecurity=true";
+        try{
+            String url = "jdbc:sqlserver://localhost:1433;" + "databaseName=StudentDatabase;integratedSecurity=true";
+            //String url ="jdbc:sqlserver://GREENANGEL\\;databaseName=StudentDatabase;integratedSecurity=true";
 
-        Connection connection = DriverManager.getConnection(url);
-        Statement statement = null;
-        statement = connection.createStatement();
+            Connection connection = DriverManager.getConnection(url);
+            Statement statement = null;
+            statement = connection.createStatement();
 
-        String sqlScript = "SELECT * FROM StudentTable";
-        ResultSet resultSet = statement.executeQuery(sqlScript);
+            String sqlScript = "SELECT * FROM StudentTable";
+            ResultSet resultSet = statement.executeQuery(sqlScript);
 
-        resultSet.next();
-        System.out.println("Name: "+ resultSet.getString("Name"));
+            resultSet.next();
+            System.out.println("Name: "+ resultSet.getString("Name"));
+        }catch(Exception e){
+            System.out.println("Can't Login");
+        }
 
-        
-
+        ArrayList<Button> ListButton = new ArrayList<Button>();
+        Button btn = new Button();
+        ListButton.add(btn);
+        btn.textProperty().setValue("Bilgisayar");
+        ListView_Department.getItems().add(btn);
+        btn.textProperty().setValue("Yazilim");
+        ListView_Department.getItems().add(btn);
 
 
 
